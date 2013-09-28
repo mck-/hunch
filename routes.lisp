f44cdee5752eb63daad7b5bac15159f41ed1b024
Marc Kuo <kuomarc2@gmail.com>
2013-09-27 20:57:47 -0700

    remove obsolete format and return string directly

--- routes.lisp
+++ routes.lisp
@@ -5,9 +5,9 @@
 (define-easy-handler (index :uri "/index" :default-request-type :get) ()
   (log-message* :info "GET on index ------ ")
   (with-json-response
-    (format nil "Hello World")))
+    "Hello World"))
 
 (define-easy-handler (echo :uri "/echo" :default-request-type :post) ()
-  (log-message* :info "POST on index ------ ")
+  (log-message* :info "POST on echo ------ ")
   (with-json-response
-    (format nil (post-body))))
+    (post-body)))
